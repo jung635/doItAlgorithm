565f0a065d51cee42def04ec54bc4f8343135747
sunju <sunju635@naver.com>
2023-01-30 22:14:06 +0900

    ch1 commit

---
+++ algorithm/src/ch1/Baekjoon_1750.java
@@ -0,0 +1,16 @@
+package ch1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baekjoon_1750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        String input = br.readLine();
+
+        Arrays.stream(input.split("")).sorted().forEach(System.out::println);
+    }
+}
