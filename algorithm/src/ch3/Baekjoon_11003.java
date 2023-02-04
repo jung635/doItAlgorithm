8377f8036c7b49199e186b1890321a3eba8d41bc
sunju <sunju635@naver.com>
2023-02-04 19:16:15 +0900

    ch3 (Done)

---
+++ algorithm/src/ch3/Baekjoon_11003.java
@@ -0,0 +1,65 @@
+package ch3;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon_11003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]); //N개의 개수
+        int L = Integer.parseInt(input[1]); //범위
+        //result Node(index, value) firstIndex i-L
+        // 1 => 1 0 -3
+        // 1 5 => 1 0 -2
+        // 1 2 => 1 0 -1
+        // 2 3 => 2 0 0
+        // 2 3 6 => 2 2 1
+        // 2 => 2 2 2
+        // 2 3 => 2 5 3
+        // 2 3 7 => 2 5 4
+        // 3 3 => 3 5 5
+        // 3 5 => 3 6 6
+        // 2 => 2 8 7
+        // 2 => 2 8 8
+        StringTokenizer st = new StringTokenizer(br.readLine()); //1 5 2 3 6 2 3 7 3 5 2 6
+        Deque<Node> dq = new LinkedList();
+        for(int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            while(!dq.isEmpty() && now < dq.getLast().value) dq.removeLast();
+            Node node = new Node(i, now);
+            dq.addLast(node);
+
+            if(dq.getFirst().index == i - L) {
+                dq.removeFirst();
+            }
+
+            bw.write(dq.getFirst().value + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static class Node {
+        int index;
+        int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "index=" + index +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+}
+
+
--- algorithm/src/ch3/Baekjoon_12891.java
+++ algorithm/src/ch3/Baekjoon_12891.java
@@ -3,48 +3,60 @@ package ch3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Baekjoon_1253 {
-    
+public class Baekjoon_12891 {
+    static final String[] DNA_MAND_STR = {"A", "C", "G", "T"};
+    static int[] CUR_Count = new int[DNA_MAND_STR.length];
+    static int CUR_COUNT_CHECK = 0;
+
+    public static int[] MAND_COUNT = new int[DNA_MAND_STR.length]; // 포함되어야 할 ACGT 최소개수
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // N 개의 수
-        long nums[] = new long[N];
+        String[] SP = br.readLine().split(" ");
+        int S = Integer.parseInt(SP[0]); // DNA 문자열의 길이
+        int P = Integer.parseInt(SP[1]); // 비밀번호로 사용할 부분 문자열의 길이
+        String[] DNA = br.readLine().split(""); //DNA 문자열
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i< N; i++) {
-            nums[i] = Long.parseLong(st.nextToken());
-        }
-        Arrays.sort(nums);
-        int answer = 0; // 만들 수 있는 갑옷의 수
-
-
-        for(int i = 0; i < N; i++) {
-            int start = 0;
-            int end = N - 1;
-            long M = nums[i];
-            long sum = 0;
-            while (start < end){
-                sum = nums[start] + nums[end];
-                if(sum == M) {
-                    if(i == start) start++;
-                    else if(i == end) end--;
-                    else {
-                        answer++;
-                        break;
-                    }
-                }else if(sum > M) {
-                    end--;
-                }else if(sum < M) {
-                    start++;
-                }
-            }
+        for(int i = 0; i < DNA_MAND_STR.length; i++) {
+            MAND_COUNT[i] = Integer.parseInt(st.nextToken());
+            if(MAND_COUNT[i] == 0) CUR_COUNT_CHECK ++;
         }
 
+        int answer = 0;
+        for(int i = 0; i < P; i++) {
+            move(DNA[i], 1);
+        }
+        if(CUR_COUNT_CHECK == DNA_MAND_STR.length) answer ++;
 
+        //1. 이동 될 때, 양쪽 아이템을 Deque에서 추가, 삭제한다.
+        //2. 추가, 삭제되는 아이템이 ACGT인지 체크하고, 개수를 조절한다.
+        //3. 조절 된 개수가 포함되어야 할 ACGT 최소개수에 부합하는지 확인한다.
+        for(int i = P; i < S; i++) {
+            move(DNA[i], 1);
+            move(DNA[i-P], -1);
+            if(CUR_COUNT_CHECK == DNA_MAND_STR.length) answer ++;
+        }
         System.out.println(answer);
+        br.close();
+    }
 
+    private static void move(String item, int addCnt) {
+        for(int i = 0; i < DNA_MAND_STR.length; i++) {
+            if(DNA_MAND_STR[i].equals(item)) {
+                if(addCnt > 0) {
+                    CUR_Count[i] += addCnt;
+                    if(CUR_Count[i] == MAND_COUNT[i]) CUR_COUNT_CHECK += addCnt;
+                }else {
+                    if(CUR_Count[i] == MAND_COUNT[i]) CUR_COUNT_CHECK += addCnt;
+                    CUR_Count[i] += addCnt;
+                }
+
+                break;
+            }
+
+        }
     }
 
 }
